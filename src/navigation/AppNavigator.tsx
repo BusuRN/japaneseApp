729522f73c34242f2d41316fc7f66011de596da1
Busuioc Andrei <andreibusuioc@Busuiocs-MacBook-Pro.local>
2024-05-13 22:33:58 +0300

    installed navigation and initial packages

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from '../screens/DetailsScreen';
+
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Details" component={DetailsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default AppNavigator
+
+const styles = StyleSheet.create({})
