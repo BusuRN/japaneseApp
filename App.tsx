c902c3fbd657cebec003463ea6863a6f89ea2174
Busuioc Andrei <andreibusuioc@Busuiocs-MacBook-Pro.local>
2025-03-04 13:02:02 +0200

    fixed status bar colors

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import React, {useEffect} from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import SplashScreen from 'react-native-splash-screen';
+import {StatusBar, StyleSheet, View} from 'react-native';
 
 function App(): React.JSX.Element {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
 
-  return <AppNavigator />;
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor={'red'} />
+      <AppNavigator />
+    </View>
+  );
 }
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- android/app/src/main/java/com/japaneseapp/MainActivity.kt
+++ android/app/src/main/java/com/japaneseapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.japaneseapp
 
+import android.os.Build;
 import android.os.Bundle;
 import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity
@@ -7,9 +8,32 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+import android.graphics.Color;
+import android.view.View;
+import android.view.Window;
+import android.content.pm.ActivityInfo
+
 class MainActivity : ReactActivity() {
 
    override fun onCreate(savedInstanceState: Bundle?) {
+requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    val w: Window = window
+    w.statusBarColor = Color.TRANSPARENT
+    w.navigationBarColor = Color.TRANSPARENT
+    w.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.O) {
+        w.navigationBarColor = Color.parseColor("#50000000")
+    } else {
+        w.navigationBarColor = Color.TRANSPARENT
+    }
+
+    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
+        w.statusBarColor = Color.parseColor("#50000000")
+    } else {
+        w.statusBarColor = Color.TRANSPARENT
+    }
+
     SplashScreen.show(this)
     super.onCreate(null)
   }
