6d08fbc890ebe999844d479c9072dfb1134aba3b
Busuioc Andrei <andreibusuioc@Busuiocs-MacBook-Pro.local>
2025-03-04 12:40:36 +0200

    added splash screen icon

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return <AppNavigator />;
 }
 export default App;
