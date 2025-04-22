82d1cae22881bfc83c12560e85fd1de8220256a3
Busuioc Andrei <andreibusuioc@Busuiocs-MacBook-Pro.local>
2025-04-22 11:15:52 +0300

    read me file updated

--- src/screens/PracticeScreen.tsx
+++ src/screens/PracticeScreen.tsx
@@ -1,7 +1,7 @@
-import {Pressable, StyleSheet, Text, View} from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
-const PracticeScreen = ({navigation}) => {
+const PracticeScreen = ({ navigation }) => {
   return (
     <View>
       <Pressable
